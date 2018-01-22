f72273c81600ce7d2da0b0673ac27aa419b502b0
Stephen Buchanan <stephen.buchanan@random.nu>
2018-01-22 16:06:12 +0100

    interaction using registered component

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 require('aframe');
 require('aframe-environment-component');
+
+console.log('hello world');
+
+AFRAME.registerComponent('scale-on-mouseenter', {
+  schema: {
+    to: {default: '2.1 2.1 2.1'}
+  },
+  init: function () {
+    var data = this.data;
+    this.el.addEventListener('mouseenter', function () {
+      this.setAttribute('scale', data.to);
+    });
+  }
+});
