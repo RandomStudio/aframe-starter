281c8f6e942a7f8f48c8dcd2ee171658240d7b15
Stephen Buchanan <stephen.buchanan@random.nu>
2018-01-23 09:44:16 +0100

    comment in index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('aframe-environment-component');
 
 console.log('hello world');
 
+// Below is not actually used, but just to demonstrate that components can be registered externally to the index.html
+
 AFRAME.registerComponent('scale-on-mouseenter', {
   schema: {
     to: {default: '2.1 2.1 2.1'}
