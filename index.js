8c43ad71e05a539a1a2aadb0b72ec1fff913cea7
Stephen Buchanan <stephen.buchanan@random.nu>
2018-01-23 10:59:33 +0100

    starting point (and downloaded audio file)

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
 require('aframe');
-require('aframe-environment-component');
-
-console.log('hello world');
-
-// Below is not actually used, but just to demonstrate that components can be registered externally to the index.html
-
-AFRAME.registerComponent('scale-on-mouseenter', {
-  schema: {
-    to: {default: '2.1 2.1 2.1'}
-  },
-  init: function () {
-    var data = this.data;
-    this.el.addEventListener('mouseenter', function () {
-      this.setAttribute('scale', data.to);
-    });
-  }
-});
