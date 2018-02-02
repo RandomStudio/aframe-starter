32be2a882f0bfedf2c3fd13af574bb2b60d99b28
Stephen Buchanan <stephen.buchanan@random.nu>
2018-02-02 16:23:45 +0100

    boilerplate demonstrating component registration

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ require('aframe');
 require('aframe-template-component');
 require('aframe-layout-component');
 require('aframe-event-set-component');
+
+AFRAME.registerComponent('set-image', {
+  schema: {
+
+  },
+
+  init: function () {
+
+  }
+});
